0b7f20d9339cc2d4282180497281d28126dfc330
demo. <demospaintct@gmail.com>
2026-06-12 19:47:47 +0200

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,7 @@
-import { PlayerProvider } from '@/lib/PlayerContext'
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <PlayerProvider>
-      {children}
-    </PlayerProvider>
-  )
+  return <>{children}</>
 }
