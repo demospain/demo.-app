3d86d7aed7a68c92c12e987f59f49e656a7209eb
demo. <demospaintct@gmail.com>
2026-06-12 20:03:24 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
-import { PlayerProvider } from '@/lib/PlayerContext'
-import GlobalPlayer from '@/components/GlobalPlayer'
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <PlayerProvider>
-      <div className="min-h-screen bg-[#0d0d0f]">
-        {children}
-        <GlobalPlayer/>
-      </div>
-    </PlayerProvider>
-  )
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
 }
