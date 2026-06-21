9d3de0419134c524877fcf111aacc15053f41ca9
demo. <demospaintct@gmail.com>
2026-06-21 23:29:08 +0200

    Update ClientProviders.tsx

--- components/ClientProviders.tsx
+++ components/ClientProviders.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect } from 'react'
 import { PlayerProvider } from '@/lib/PlayerContext'
 
 export default function ClientProviders({
@@ -7,6 +8,12 @@ export default function ClientProviders({
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {})
+    }
+  }, [])
+
   return (
     <PlayerProvider>
       {children}
